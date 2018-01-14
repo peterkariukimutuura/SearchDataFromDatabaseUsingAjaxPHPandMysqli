1f00acc941bfdcf0e5f1e3ece32b4afd15197736
peterkariukimutuura <kariukipeter30@gmail.com>
2018-01-14 23:35:58 +0300

    logic fix

--- postdata.php
+++ postdata.php
@@ -4,13 +4,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$name=sanitizeUserInput($_POST['name']);
 	$gender=sanitizeUserInput($_POST['gender']);
 	$dateofbirth=sanitizeUserInput($_POST['dateofbirth']);
-	//echo $name."....".$gender."....".$dateofbirth;
-	// $name=$_POST['name'];
-	// $gender=$_POST['gender'];
-	// $dateofbirth=$_POST['dateofbirth'];
-
 	if (!empty($name)&&!empty($gender)&&!empty($dateofbirth)) {
+
 		$sql="INSERT INTO `users` (`name`, `gender`, `dateofbirth`) VALUES ( '$name', '$gender', '$dateofbirth')";
+		if (mysqli_num_rows(mysqli_query($conn,"SELECT * FROM users WHERE name='$name'"))>0) {
+			return;		}
 		if (mysqli_query($conn,$sql)) {
 			echo 'Record Inserted Successfully';
 		}else{
